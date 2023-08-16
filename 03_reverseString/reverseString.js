db4de3457a8f26b4b5f90a693c3122fda3ce74ef
Stefan Konzelmann <stefa.konzelmann@gmx.de>
2023-08-16 11:59:57 +0200

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let input = [...string];
+  let inputReversed = "";
 
+  for (let i = input.length - 1; i >= 0; i--) {
+    console.log(input[i]);
+    inputReversed += input[i];
+  }
+  return inputReversed;
 };
+
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
