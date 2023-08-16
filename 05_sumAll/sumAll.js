7cede5239e7206ac4f4fe0ef3c301b3577a3349d
Stefan Konzelmann <stefa.konzelmann@gmx.de>
2023-08-16 13:25:21 +0200

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
 
+  let lower;
+  let upper;
+  let sum = 0;
+
+  if (num1 > num2) {
+    lower = num2;
+    upper = num1;
+  } else {
+    lower = num1;
+    upper = num2;
+  }
+
+  for (i = lower; i <= upper; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
