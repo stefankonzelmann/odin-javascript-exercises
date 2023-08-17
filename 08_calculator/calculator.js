6fc14f8669ffb9c9f57c4d69f916107480f0a1db
Stefan Konzelmann <stefan.konzelmann@gmx.de>
2023-08-17 14:38:37 +0200

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,39 @@
-const add = function() {
+const add = function (...args) {
+  return args.reduce((acc, cur) => acc + cur, 0)
+};
+
+const subtract = function(start, ...args) {
+  return args.reduce((acc, cur) => acc - cur, start)
 	
 };
 
-const subtract = function() {
+const sum = function(...args) {
+	return Number(args.flat().reduce((acc, cur) => acc + cur, 0))
+};
+
+const multiply = function(...args) {
+  start= args.flat()[0]
+  args = args.flat().splice(1)
+  return args.flat().reduce((acc, cur) => acc * cur, start)
+};
+
+const power = function(base, exp) {
+  return base**exp
+};
+
+const factorial = function(num) {
+  if (num < 0) {
+    return 'num must not be negative'
+  }
+  if (num <= 1) {
+    // Both 1! and 0! are defined as 1
+    return 1
+  }
+  return num * factorial(num -1)
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+console.log(factorial(3))
 
 // Do not edit below this line
 module.exports = {
