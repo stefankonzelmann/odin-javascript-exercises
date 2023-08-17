108ce71a10a0b233cddc9a72efd3eff4b561318f
Stefan Konzelmann <stefan.konzelmann@gmx.de>
2023-08-17 16:08:06 +0200

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach(person => {
+        if (person.yearOfDeath) {
+            person.age = person.yearOfDeath - person.yearOfBirth
+        } else {
+            person.age = 2023 - person.yearOfBirth
+        }
+    });
+    oldest = people.sort((a, b) => a.age > b.age ? -1 : 1)[0]
+    return oldest;
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+console.log(findTheOldest(people))
 
 // Do not edit below this line
 module.exports = findTheOldest;
