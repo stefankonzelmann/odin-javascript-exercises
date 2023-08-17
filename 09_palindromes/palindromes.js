3300b7466e7ea028160525b5b331bd283efe438b
Stefan Konzelmann <stefan.konzelmann@gmx.de>
2023-08-17 15:16:44 +0200

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const remove = [" ", ",", ".", "!"]
+    const inputClean = Array.from(string.toLowerCase()).filter(char => !remove.includes(char)).join("")
+    return inputClean === inputClean.split("").reverse().join("")
 };
 
 // Do not edit below this line
