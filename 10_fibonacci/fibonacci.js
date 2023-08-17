ba483cfb3a9859452b6bfe114d2723eb63148ae9
Stefan Konzelmann <stefan.konzelmann@gmx.de>
2023-08-17 15:43:58 +0200

    Solve 10_fibonacci with recursion

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(fNum) {
+    if (fNum < 0 ) return 'OOPS'
+    if (fNum <= 2) return 1
+    return fibonacci(fNum -1) + fibonacci (fNum -2)
 };
 
 // Do not edit below this line
